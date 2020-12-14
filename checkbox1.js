1f1cb0c41a4abb5c0c1473848ddf279d42ba266c
Rishit Dutta <65221538+rishitdutta@users.noreply.github.com>
2020-12-14 11:25:52 +0530

    Create checkbox1.js

---
+++ checkbox1.js
@@ -0,0 +1,8 @@
+function toggle()
+{
+    var checkbox = document.getElementById("checkbox");
+    if (checkbox.checked==0){
+      alert("Please check the checkbox before continuing");
+      return false;
+  }
+}
