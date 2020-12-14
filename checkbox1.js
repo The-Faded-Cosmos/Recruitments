80a2ea81c4b23ac8bf9ad558e52c785236b88050
Rishit Dutta <65221538+rishitdutta@users.noreply.github.com>
2020-12-14 12:09:05 +0530

    Delete checkbox1.js

--- checkbox1.js
+++
@@ -1,8 +0,0 @@
-function toggle()
-{
-    var checkbox = document.getElementById("checkbox");
-    if (checkbox.checked==0){
-      alert("Please check the checkbox before continuing");
-      return false;
-  }
-}
